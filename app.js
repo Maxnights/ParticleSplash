0de4c0b4b084d6306f2abbe379e89930b4c5c2f6
Maxnights <109459573+Maxnights@users.noreply.github.com>
2025-05-14 20:59:13 +0400

    Introduce Particle class and simple multiple particle animation in js/app.js

--- app.js
+++ app.js
@@ -8,12 +8,35 @@ function resize() {
 window.addEventListener('resize', resize);
 resize();
 
-function loop() {
+class Particle {
+  constructor() {
+    this.x = Math.random()*canvas.width;
+    this.y = Math.random()*canvas.height;
+    this.speedX = (Math.random()-0.5)*2;
+    this.speedY = (Math.random()-0.5)*2;
+  }
+  update() {
+    this.x += this.speedX;
+    this.y += this.speedY;
+    if(this.x<0||this.x>canvas.width) this.speedX*=-1;
+    if(this.y<0||this.y>canvas.height) this.speedY*=-1;
+  }
+  draw() {
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, 2, 0, Math.PI*2);
+    ctx.fill();
+  }
+}
+
+let particles = Array.from({length:50}, () => new Particle());
+
+function animate() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   ctx.fillStyle = '#fff';
-  ctx.beginPath();
-  ctx.arc(canvas.width/2, canvas.height/2, 5, 0, Math.PI*2);
-  ctx.fill();
-  requestAnimationFrame(loop);
+  particles.forEach(p => {
+    p.update();
+    p.draw();
+  });
+  requestAnimationFrame(animate);
 }
-loop();
+animate();
